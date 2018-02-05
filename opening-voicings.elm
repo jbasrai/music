a9546615d5036a964311b6bea40e5a5abf1960db
Jauhar Basrai <jbasrai@smash.gg>
2018-02-05 16:16:20 -0500

    Create table

--- opening-voicings.elm
+++ opening-voicings.elm
@@ -1,4 +1,6 @@
-import Html exposing (Html, div, text, span)
+import Html exposing (Html, div, text, span, button)
+import Dict
+import Debug exposing (log)
 
 
 
@@ -11,14 +13,61 @@ main =
   }
 
 
+explode xs ys =
+  List.map (\x -> List.map (\y -> (x, y)) ys) xs
+    |> List.concat
+
 
 -- INIT
+type alias Target =
+  { root : String
+  , chord : String
+  , lead : Int
+  }
 
+
+-- allTargets : List Target
+allTargets =
+  let
+      roots : List String
+      roots =
+        [ "A"
+        , "Aflat"
+        , "B"
+        , "Bflat"
+        , "C"
+        , "D"
+        , "Dflat"
+        , "E"
+        , "Eflat"
+        , "F"
+        , "G"
+        , "Gflat"
+        ]
+
+      voicings : List (String, List Int)
+      voicings =
+        [ ("Maj6", [1, 3, 5, 6])
+        , ("min6", [1, 3, 5, 6])
+        ]
+
+      explodedVoicings : List (String, Int)
+      explodedVoicings =
+        List.map (\(chord, lead) -> ([chord], lead)) voicings
+        |> List.map (\(chord, lead) -> (explode chord lead))
+        |> List.concat
+
+      explodedEverything : List Target
+      explodedEverything =
+        explode roots explodedVoicings
+        |> List.map (\(root, (chord, lead)) -> Target root chord lead)
+
+  in
+      explodedEverything
 
 type alias Model =
-  { root: String
-  , chord: String
-  , lead: String
+  { target : Target
+  , table : List Target
   }
 
 
@@ -27,7 +76,11 @@ type Msg = Nil
 
 init : (Model, Cmd Msg)
 init =
-  (Model "Bflat" "Maj7" "7", Cmd.none)
+  let
+    target =
+      Target "Bflat" "Maj7" 7
+  in
+    (Model target [], Cmd.none)
 
 
 
@@ -45,11 +98,16 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ span [] [text (model.root ++ " ")]
-    , span [] [text (model.chord ++ " ")]
-    , span [] [text (model.lead ++ " ")]
-    ]
+  let
+      target =
+        model.target
+  in
+    div []
+      [ span [] [text (target.root ++ " ")]
+      , span [] [text (target.chord ++ " ")]
+      , span [] [text ((toString target.lead) ++ " ")]
+      , button [] [text "next"]
+      ]
 
 
 
