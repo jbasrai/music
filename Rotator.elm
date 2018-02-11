7365a7f90b063af931a8be9a0557ef4f299fbe91
Jauhar Basrai <jbasrai@smash.gg>
2018-02-11 01:44:31 -0500

    Add timer and playback controls

--- Rotator.elm
+++ Rotator.elm
@@ -6,6 +6,7 @@ import Random
 import Random.List
 import Note exposing (Note)
 import Maybe
+import Time exposing (Time)
 
 -- TODO: pseudo random
 
@@ -22,12 +23,19 @@ main =
 -- INIT
 type alias Model =
   { key : Note
+  , lastTick : Time
+  , shouldSync : Bool
+  , isPaused : Bool
   }
 
 
 init : (Model, Cmd Msg)
 init =
-  ( Model Note.Bb
+  ( { key = Note.Bb
+    , lastTick = 0
+    , shouldSync = False
+    , isPaused = False
+    }
   , Random.generate NextNote (randomNote Note.Bb)
   )
 
@@ -36,7 +44,7 @@ init =
 -- UPDATE
 
 
-type Msg = RandomNote | NextNote Note
+type Msg = RandomNote | NextNote Note | Tick Time | Sync | Pause
 
 randomNote : Note -> Random.Generator Note
 randomNote exclude =
@@ -53,7 +61,36 @@ update msg model = case msg of
     )
 
   NextNote note ->
-    ( Model note
+    ( { model | key = note }
+    , Cmd.none
+    )
+
+  Tick time ->
+    if (not model.isPaused
+      && (
+        model.shouldSync
+        || Time.inSeconds (time - model.lastTick) >= 3
+      )
+    ) then
+      ( { model |
+            lastTick = time,
+            shouldSync = False
+        }
+      , Random.generate NextNote (randomNote model.key)
+      )
+    else 
+      (model, Cmd.none)
+
+  Sync ->
+    ( { model |
+          shouldSync = True,
+          isPaused = False
+      }
+    , Cmd.none
+    )
+
+  Pause ->
+    ( { model | isPaused = True }
     , Cmd.none
     )
 
@@ -65,7 +102,9 @@ view : Model -> Html Msg
 view model =
   div []
     [ h1 [] [text (Note.toString model.key)]
-    , button [onClick RandomNote] [text "next"]
+    , div [] [text (if (model.isPaused) then "Paused" else "Playing")]
+    , button [onClick Sync] [text "sync"]
+    , button [onClick Pause] [text "pause"]
     ]
 
 
@@ -75,4 +114,4 @@ view model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  Time.every (50 * Time.millisecond) Tick
